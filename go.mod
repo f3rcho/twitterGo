6e2808ddfe7b74de0e78e8f467048bf834bbf3a9
Fernando Cordero <fernando.corderobastidas@cencosud.cl>
2023-10-22 23:44:07 -0300

    ADD: main - aws lmabda pkg - validation

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/f3rcho/twitterGo
 
 go 1.21.3
+
+require github.com/aws/aws-lambda-go v1.41.0
--- main.go
+++ main.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	"context"
+	"os"
 
+	"github.com/aws/aws-lambda-go/events"
+	lambda "github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(ExecuteLambda)
+
+}
+
+func ExecuteLambda(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	var res *events.APIGatewayProxyResponse
+
+	if !validateParams() {
+		res = &events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body:       "Missing parameters SecretName, BucketName, UrlPrefix",
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
+		}
+	}
+	return res, nil
+}
+
+func validateParams() bool {
+	_, bringParams := os.LookupEnv("SecretName")
+	if !bringParams {
+		return bringParams
+	}
+	_, bringParams = os.LookupEnv("BucketName")
+	if !bringParams {
+		return bringParams
+	}
+	_, bringParams = os.LookupEnv("UrlPrefix")
+	if !bringParams {
+		return bringParams
+	}
+	return bringParams
 }
