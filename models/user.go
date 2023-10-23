57c641859f81daeca98672119c26c53654319902
Fernando Cordero <fernando.corderobastidas@cencosud.cl>
2023-10-23 12:39:21 -0300

    ADD: user entity

---
+++ models/user.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Name      string             `bson:"name" json:"name,omitempty"`
+	LastName  string             `bson:"lastName" json:"lastName,omitempty"`
+	BirthDay  time.Time          `bson:"birthDay" json:"birthDay,omitempty"`
+	Email     string             `bson:"email" json:"email"`
+	Password  string             `bson:"password" json:"password,omitempty"`
+	Avatar    string             `bson:"avatar" json:"avatar,omitempty"`
+	Banner    string             `bson:"banner" json:"banner,omitempty"`
+	Biography string             `bson:"biography" json:"biography,omitempty"`
+	Location  string             `bson:"location" json:"location,omitempty"`
+	Website   string             `bson:"website" json:"website,omitempty"`
+}
