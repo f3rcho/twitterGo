217a2013dd3952dbb6349e5a860b950ac8a0ed43
Fernando Cordero <fernando.corderobastidas@cencosud.cl>
2023-10-28 12:18:28 -0300

    ADD: login in router

---
+++ jwt/jwt.go
@@ -0,0 +1,33 @@
+package jwt
+
+import (
+	"context"
+	"time"
+
+	"github.com/f3rcho/twitterGo/models"
+	jwt "github.com/golang-jwt/jwt/v5"
+)
+
+func GenerateJWT(ctx context.Context, u models.User) (string, error) {
+	jwtSign := ctx.Value(models.Key("jwtSign")).(string)
+	myKey := []byte(jwtSign)
+
+	payload := jwt.MapClaims{
+		"email":     u.Email,
+		"name":      u.Name,
+		"lastName":  u.LastName,
+		"birthDay":  u.BirthDay,
+		"location":  u.Location,
+		"webSite":   u.Website,
+		"biography": u.Biography,
+		"_id":       u.ID.Hex(),
+		"exp":       time.Now().Add(time.Hour * 24).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
+	tokenStr, err := token.SignedString(myKey)
+	if err != nil {
+		return tokenStr, err
+	}
+	return tokenStr, nil
+}
---
+++ routers/login.go
@@ -0,0 +1,71 @@
+package routers
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/f3rcho/twitterGo/db"
+	"github.com/f3rcho/twitterGo/jwt"
+	"github.com/f3rcho/twitterGo/models"
+)
+
+func Login(ctx context.Context) models.ResposeAPI {
+	var u models.User
+	var r models.ResposeAPI
+	r.Status = 400
+
+	body := ctx.Value(models.Key("body")).(string)
+	err := json.Unmarshal([]byte(body), &u)
+	if err != nil {
+		r.Message = "Invalid User or Password" + err.Error()
+		return r
+	}
+	if len(u.Email) == 0 {
+		r.Message = ""
+		return r
+	}
+	userData, exists := db.TryLogin(u.Email, u.Password)
+	if !exists {
+		r.Message = "Invalid User or Password"
+		return r
+	}
+	jwtKey, err := jwt.GenerateJWT(ctx, userData)
+	if err != nil {
+		r.Message = "An error has occured trying to generate token" + err.Error()
+		return r
+	}
+
+	resp := models.LoginResponse{
+		Token: jwtKey,
+	}
+	token, errToken := json.Marshal(resp)
+	if errToken != nil {
+		r.Message = "An error has occured trying to generate token" + errToken.Error()
+		return r
+	}
+
+	cookie := &http.Cookie{
+		Name:    "token",
+		Value:   jwtKey,
+		Expires: time.Now().Add(24 * time.Hour),
+	}
+	cookieString := cookie.String()
+
+	res := &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       string(token),
+		Headers: map[string]string{
+			"Content-Type":                "application/json",
+			"Access-Control-Allow-Origin": "*",
+			"Set-Cookie":                  cookieString,
+		},
+	}
+	r.Status = 200
+	r.Message = string(token)
+	r.CustomResponse = res
+
+	return r
+}
